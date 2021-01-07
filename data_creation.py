fe343ade8c7679e7c709403faa51a386ad6f87d9
Spiridoula <smenounou@gmail.com>
2021-01-07 15:45:19 +0200

    first commit

---
+++ data_creation.py
@@ -0,0 +1,61 @@
+import numpy as np
+from numpy import load, save
+from PIL import Image
+import matplotlib.pyplot as plt
+from shuffle import shuffle_in_unison
+import imageio
+import matplotlib.pyplot as plt
+import cv2
+from scipy.io import loadmat
+from skimage import transform, io, img_as_ubyte
+
+
+def read_data(im_size, rsp_size):
+    #speckles_patterns_val = np.zeros((300, 500, 700))
+    #for i in range(1, 301):
+    #    speckles_patterns = loadmat(('C://Users//spime//OneDrive//Υπολογιστής//train//symbol_Responses//Symbol_response_'+str(k)+'_'+str(i)+'.mat'))
+    #    speckles_patterns_val[i-1] = speckles_patterns['a2']
+    dim_img = (im_size, im_size)
+    dim_rsp = (rsp_size, rsp_size)
+    for k in range(8, 11):
+        for i in range(1, 259):
+            speckle = loadmat('C://Users//spime//OneDrive//Υπολογιστής//train//symbol_Responses//Symbol_response_'+str(k)+'_'+str(i)+'.mat')
+            speckle_val = speckle['a2']
+            imageio.imwrite('speckle.jpg',speckle_val) 
+            image = imageio.imread('speckle.jpg')
+            # resize image
+            resized = cv2.resize(image, dim_img, interpolation = cv2.INTER_CUBIC)
+            #resized = imresize(image, dim, interp='bilinear')
+            imageio.imwrite('C://Users//spime//OneDrive//Υπολογιστής//train//symbol_Responses_resized//'+str(k)+'//symbol_'+str(k)+'_'+str(i)+'.jpg', resized)
+
+        for i in range(1, 259):
+            symbol = imageio.imread('C://Users//spime//OneDrive//Υπολογιστής//train//'+str(k)+'//symbol_'+str(i)+'.jpg')
+            # resize image
+            resized = cv2.resize(symbol, dim_rsp, interpolation = cv2.INTER_CUBIC)
+            #resized = transform.resize(symbol, dim, mode='symmetric', preserve_range=True)
+            imageio.imsave('C://Users//spime//OneDrive//Υπολογιστής//train//'+str(k)+'_resized//symbol_'+str(k)+'_'+str(i)+'.jpg', resized)
+
+def create_the_dataset(rsp_size, im_size):
+    speckle_array = np.zeros((3, 258, rsp_size, rsp_size)) 
+    symbol_array = np.zeros((3, 258, im_size, im_size))
+
+    t=0
+    for k in range(8, 11):
+        for i in range(1, 259):
+            speckle = Image.open('C://Users//spime//OneDrive//Υπολογιστής//train//symbol_Responses_resized//'+str(k)+'//symbol_'+str(k)+'_'+str(i)+'.jpg')
+            speckle = np.asarray(speckle)
+            speckle_array[t,i-1] = speckle
+            symbol = Image.open('C://Users//spime//OneDrive//Υπολογιστής//train//'+str(k)+'_resized//symbol_'+str(k)+'_'+str(i)+'.jpg')
+            symbol = np.asarray(symbol)
+            symbol_array[t,i-1] = symbol
+        t=t+1
+
+    speckle_array = speckle_array.astype(int)
+    symbol_array = symbol_array.astype(int)
+    print(speckle_array.shape)
+    print(symbol_array.shape)
+
+    for i in range(speckle_array.shape[0]):
+        speckle_array_case, symbol_array_case = shuffle_in_unison(speckle_array[i], symbol_array[i])
+        save('speckle_array_case'+str(i)+'.npy', speckle_array_case)
+        save('symbol_array_case'+str(i)+'.npy', symbol_array_case)
---
+++ model_structure.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+#from tensorflow import multi_gpu_model
+import keras
+from keras.models import Sequential, Model, model_from_json
+from keras.layers import Dense, Activation, Dropout, Flatten, Reshape
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from tensorflow.keras.callbacks import TensorBoard
+
+def create_model(img_input, input_shape, input_shape_test):
+    print(input_shape[0])
+    power = find_power(input_shape[0])
+    nb_filter = 2**(power-1)
+    print(power)
+    print(nb_filter)
+    # x = Sequential()(img_input)
+    x = Conv2D(nb_filter, 1, activation = 'relu', input_shape=input_shape)(img_input)
+    x = MaxPooling2D(pool_size = (2,2))(x)
+    x = Dropout(0.25)(x)
+    x = Conv2D(32, 1, activation = 'relu')(x)
+    x = Reshape((1, 32*32*32), input_shape=(32, 32, 32))(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.25)(x)
+    x = Dense(784, activation='relu')(x)
+    x = Dropout(0.25)(x)
+    x = Dense(784, activation='relu')(x)
+    x = Dropout(0.25)(x)
+    x = Reshape((28, 28, 1), input_shape=(1, 784))(x)
+    x = Conv2D(32, 1, activation = 'relu', input_shape=input_shape_test)(x)
+    x = Dropout(0.25)(x)
+    x = Conv2D(16, 1, activation = 'relu')(x)
+    x = Dropout(0.05)(x)
+    x = Conv2D(1, 1, activation = 'relu')(x)
+    model = Model(img_input, x, name='reconstruction')
+    
+    return model
+
+
+def find_power(input_shape):
+    power = 0
+    i = 2**power
+    while(i<input_shape):
+        power += 1
+        i = 2**power
+        
+    return power
+
+
+
+        
